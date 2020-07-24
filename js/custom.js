ae9be21445bc5405e721e52dea2987ddabe4255e
Asim-v <emoxic.sama@gmail.com>
2020-07-24 15:30:49 -0500

    custom implementation y mejorado visual

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,10 @@ var rectboxX = 130,
 
 function updatePrice(price_change) {
   var regExp = /(\=|\+|\-)(\d+)/;
-  var result, result_sign, result_no;
+  var result, result_sign, result_no, price;
+
+  currentPrice = parseInt(document.getElementById("price").innerHTML);
+  
 
   /* Use Regular Expression to decide input
      undefined = no change
@@ -19,22 +22,28 @@ function updatePrice(price_change) {
      '+50' = add 50 baht
      '-50' = decrease 50 baht
   */
+  // console.log(regExp.exec(price_change));
   if ((result = regExp.exec(price_change)) != null) {
       if (result.index === regExp.lastIndex) {
           regExp.lastIndex++;
       }
       result_sign = result[1];
-      result_no = result[2];
+      result_no = parseInt(result[2]);
   }
   
-  if (result_sign == '=') {
-    price = price_change;
-  } else if (result_sign == '+') {
-    price += price_change;
-  } else if (result_sign == '-') {
-    price -= price_change
+
+
+  if (result_sign === '='){
+    price = result_no;
+  }
+  if (result_sign === '+'){
+    price = currentPrice + result_no;
+  }
+  if (result_sign === '-'){
+    price = currentPrice - result_no;
   }
   
+  // console.log(price)
   /* Update Price */
   $('#price').html(price);
 }
@@ -75,9 +84,17 @@ $(document).ready(function() {
         // start fabricJS stuff
 
         var image = new fabric.Image(imgObj);
-        image.scale(0.5).set({
-            left: 0,
-            right: 0
+        image.scale(0.8).set({
+            right: 0,
+            left: -10,
+            top: -10,
+            angle: 0,
+            // width:620,
+            // height:500,
+            borderColor: '#03fcb1',
+            cornerColor: '#03fcb1',
+            cornerStyle:'circle',
+            cornerSize:10,            
         });
         //image.scale(getRandomNum(0.1, 0.25)).setCoords();
       
@@ -95,7 +112,7 @@ $(document).ready(function() {
 
         // end fabricJS stuff
       
-        updatePrice(select_price);
+        updatePrice("+"+String(select_price));
     }
   });
   
@@ -140,9 +157,16 @@ $(document).ready(function() {
     var inColor = $('#inputColor').val();
     
     var newText = new fabric.Text(inText, {
-      fontSize: inSize,
-      fontFamily: inFont,
-      fill: inColor
+            right: 0,
+            left: -10,
+            top: -10,
+            angle: 0,
+            // width:620,
+            // height:500,
+            borderColor: '#03fcb1',
+            cornerColor: '#03fcb1',
+            cornerStyle:'circle',
+            cornerSize:10,   
     });
     
     newText.on('selected', function() {
@@ -211,8 +235,17 @@ $(document).ready(function() {
             
             var image = new fabric.Image(imgObj);
             image.set({
-                left: 0,
-                right: 0
+              right: 0,
+              left: -10,
+              top: -10,
+              angle: 0,
+              // width:620,
+              // height:500,
+              borderColor: '#03fcb1',
+              cornerColor: '#03fcb1',
+              cornerStyle:'circle',
+              cornerSize:10,   
+
             });
           
             image.on('selected', function() {
@@ -241,6 +274,9 @@ $(document).ready(function() {
       onApprove : function() {
         var obJ = canvas.getActiveObject();
         
+        updatePrice("-"+String(obJ.itemPrice))
+        updatePrice(+Math.abs(obJ.itemPrice))
+
         // Remove from item_list
         var obJindex = item_list.indexOf(obJ);
         if (obJindex > -1) {
